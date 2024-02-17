6703ae60ccbcfd2cfd39ce3063029b0e3030768c
adarsh878856 <adarsh878856@gmail.com>
2024-02-17 19:45:18 +0530

    Feature: Implement reusable body header component

--- src/Components/Body/BodyFooter.jsx
+++ src/Components/Body/BodyFooter.jsx
@@ -1,13 +1,14 @@
+/* eslint-disable no-empty-pattern */
 /* eslint-disable react/prop-types */
-import React, { useEffect, useState } from 'react';
+import React from 'react';
 import { NavLink } from 'react-router-dom';
 import { convertDate, convertTime } from '../../utils';
-import './BodyFooter.css';
 import LazyImage from '../Common/LazyImage';
 import { useDataLayerValue } from '../../Context/DataLayer';
+import './BodyFooter.css';
 
 export default function BodyFooter({ track, added_at, index }) {
-    const [{ songs }, dispatch] = useDataLayerValue();
+    const [{}, dispatch] = useDataLayerValue();
 
     function songsHandler() {
         dispatch({ type: 'SET_SONGS', songs: track });
@@ -15,7 +16,7 @@ export default function BodyFooter({ track, added_at, index }) {
 
     return (
         <div className='bodyFooter' onClick={songsHandler}>
-            <div className='bodyFooter__rowOne col1'>
+            <div className='col1'>
                 {index && <p>{index}</p>}
                 <LazyImage
                     src={track?.album?.images[2].url || track?.album?.images[0].url}
@@ -24,22 +25,25 @@ export default function BodyFooter({ track, added_at, index }) {
                 <div>
                     <NavLink
                         title={track?.type}
-                        className='bodyFooter__name line-clamp'
+                        className='bodyFooter__name'
                         to={`/track/${track?.id}`}>
                         {track?.name}
                     </NavLink>
-                    <p className='bodyFooter__artist line-clamp'>
+                    <p className='bodyFooter__artist'>
                         {track?.artists?.map((artist) => (
-                            <span key={artist?.id} title={artist?.type} className='span hover'>
+                            <NavLink
+                                to={`/artists/${artist?.id}`}
+                                key={artist?.id}
+                                title={artist?.type}>
                                 {artist?.name}
-                            </span>
+                            </NavLink>
                         ))}
                     </p>
                 </div>
             </div>
-            <p className='span hover line-clamp col2' title={track?.album?.type}>
+            <NavLink to={`/album/${track?.album?.id}`} className='col2' title={track?.album?.type}>
                 {track?.album?.name}
-            </p>
+            </NavLink>
             <p className='col3'>{(added_at && convertDate(added_at)) || '02-14-2023'}</p>
             <p className='col4'>{convertTime(track?.duration_ms) || 12345}</p>
         </div>
