ca84beafbded7804ff2280c4987beca405642176
adarshtheki <adarshverma549@gmail.com>
2024-03-24 23:21:18 +0530

    Modify the Context to Golbal State Manage Components

--- src/Context/reducer.js
+++ src/Context/reducer.js
@@ -1,7 +1,9 @@
 export const initialState = {
     user: null,
     playLists: [],
-    artistsList: null,
+    collections: [],
+    artistsList: [],
+    recentTracks: [],
     playing: null,
     songs: null,
     // token: 'BQCYwJEC_IyUghOnY-8z9_pzi-hq_giHyVWfqFOKUr2INWZF_8oq6nHr2Fs3fnW1erWwTYLU3cUBidiLk5TZcbFt2WhQ2YuAgAsQQm54nKqJ5-KhWHFUxV1-_9khanMhOlUy_P1vaR1Xpm-2RoDjkUDknAFMyGbcG88GFLWnpqMFmq55pIe0R68R2E8N3PL97eD2yenojRuRJ5FCq-znb_ndUIFETM2WvbRA5l8gn2fqA4JgWCAIELWdes0r-XLP91g21Iett3dTe0oKh5r4GjyHaAfS7Y7-HtQBuRLBZyZ8HkM88XiUKLVDbwh0ZzQ5XIHZr1lg',
@@ -24,22 +26,32 @@ const reducer = (state, action) => {
         case 'SET_PLAYLISTS':
             return {
                 ...state,
-                playLists: action.playLists,
+                playLists: action.payload,
             };
-        case 'SET_PLAYING':
+        case 'SET_COLLECTION':
             return {
                 ...state,
-                playing: action.playing,
+                collections: action.payload,
             };
-        case 'SET_SONGS':
+        case 'SET_RECENT_TRACKS':
             return {
                 ...state,
-                songs: action.songs,
+                recentTracks: action.payload,
             };
         case 'SET_ARTISTS_LIST':
             return {
                 ...state,
-                artistsList: action.artistsList,
+                artistsList: action.payload,
+            };
+        case 'SET_SONGS':
+            return {
+                ...state,
+                songs: action.payload,
+            };
+        case 'SET_PLAYING':
+            return {
+                ...state,
+                playing: action.payload,
             };
         default:
             return state;
