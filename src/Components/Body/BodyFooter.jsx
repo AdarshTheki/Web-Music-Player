ad1d06c5af9dd2a5a9c0bd2203512af4064f48cc
adarshtheki <adarshverma549@gmail.com>
2024-03-24 23:19:52 +0530

    Modify the Body Components

--- src/Components/Body/BodyFooter.jsx
+++ src/Components/Body/BodyFooter.jsx
@@ -1,50 +1,115 @@
 /* eslint-disable no-empty-pattern */
 /* eslint-disable react/prop-types */
-import React from 'react';
+import React, { useEffect } from 'react';
 import { NavLink } from 'react-router-dom';
 import { convertDate, convertTime } from '../../utils';
 import LazyImage from '../Common/LazyImage';
 import { useDataLayerValue } from '../../Context/DataLayer';
 
-export default function BodyFooter({ track, added_at, index }) {
+export default function Data({ data = [] }) {
     const [{}, dispatch] = useDataLayerValue();
 
-    function songsHandler() {
-        dispatch({ type: 'SET_SONGS', songs: track });
-    }
+    useEffect(() => {
+        dispatch({ type: 'SET_RECENT_TRACKS', payload: data });
+    }, [dispatch, data]);
 
     return (
-        <div className='bodyFooter' onClick={songsHandler}>
-            <div className='col1'>
-                {index && <p>{index}</p>}
-                <LazyImage
-                    src={track?.album?.images[2].url || track?.album?.images[0].url}
-                    alt={track?.name}
+        <>
+            <div className='bodyFooter-header'>
+                <div style={{ flex: 2 }}>#&nbsp;&nbsp;&nbsp;Title</div>
+                <div style={{ flex: 1 }}>Album</div>
+                <div style={{ flex: 0.8 }}>Added Date</div>
+                <div style={{ flex: 0.5 }}>Duration</div>
+            </div>
+            {data?.map((item, index) => (
+                <BodyFooter
+                    key={index}
+                    track={item?.track}
+                    added_at={item?.added_at || item?.played_at}
+                    index={index + 1}
                 />
+            ))}
+        </>
+    );
+}
+
+function BodyFooter({
+    track = {
+        album: {
+            album_type: 'album',
+            id: '#',
+            images: [
+                {
+                    url: 'https://i.scdn.co/image/ab67616d0000485177eb7a5b9367de79c3b92a49',
+                },
+            ],
+            name: 'album none',
+            release_date: '2000-01-01',
+            type: 'album',
+        },
+        artists: [
+            {
+                id: '#',
+                name: 'artists none',
+                type: 'artist',
+            },
+        ],
+        duration_ms: 12345,
+        id: '#',
+        name: 'none',
+        popularity: 54,
+        preview_url: null,
+        track_number: null,
+        type: 'track',
+    },
+    added_at = '02-14-2023',
+    index,
+}) {
+    const [{}, dispatch] = useDataLayerValue();
+
+    const handleSongs = () => {
+        dispatch({ type: 'SET_SONGS', payload: track });
+    };
+
+    return (
+        <div className='bodyFooter' onClick={handleSongs}>
+            <div className='col1' style={{ flex: 2 }}>
+                {index && <p>{index}</p>}
+                <LazyImage src={track?.album?.images[0]?.url} alt={track.name} />
                 <div>
                     <NavLink
-                        title={track?.type}
+                        title={track.type}
                         className='bodyFooter__name'
-                        to={`/track/${track?.id}`}>
-                        {track?.name}
+                        to={`/track/${track.id}`}>
+                        {track.name}
                     </NavLink>
                     <p className='bodyFooter__artist'>
-                        {track?.artists?.map((artist) => (
+                        {track.artists.slice(0, 3).map((artist) => (
                             <NavLink
-                                to={`/artists/${artist?.id}`}
-                                key={artist?.id}
-                                title={artist?.type}>
-                                {artist?.name}
+                                to={`/artists/${artist.id}`}
+                                key={artist.id}
+                                title={artist.type}>
+                                {artist.name}
                             </NavLink>
                         ))}
                     </p>
                 </div>
             </div>
-            <NavLink to={`/album/${track?.album?.id}`} className='col2' title={track?.album?.type}>
-                {track?.album?.name}
-            </NavLink>
-            <p className='col3'>{(added_at && convertDate(added_at)) || '02-14-2023'}</p>
-            <p className='col4'>{convertTime(track?.duration_ms) || 12345}</p>
+            {track.album && (
+                <NavLink
+                    to={`/album/${track?.album?.id}`}
+                    style={{ flex: 1 }}
+                    className='col2'
+                    title={track?.album?.type}>
+                    {track?.album?.name}
+                </NavLink>
+            )}
+            <p className='col3' style={{ flex: 0.8 }}>
+                {convertDate(added_at)}
+            </p>
+            <p className='col4' style={{ flex: 0.5 }}>
+                {convertTime(track?.duration_ms)}
+            </p>
         </div>
     );
 }
--- src/Components/Body/BodyHeader.jsx
+++ src/Components/Body/BodyHeader.jsx
@@ -3,17 +3,16 @@ import React from 'react';
 import { convertTime, convertDate } from '../../utils';
 import { NavLink } from 'react-router-dom';
 import picture from '../../assets/songs.png';
-import IFrame from '../Common/IFrame'
 
 export default function BodyHeader({
+    album,
+    artists,
     duration_ms = 221700,
     popularity = 45,
     name = 'Liked Songs',
     description,
     length,
     owner,
-    album,
-    artists,
     type,
     images,
     followers,
@@ -21,54 +20,50 @@ export default function BodyHeader({
     const preview_url = images?.length ? images[0]?.url : album?.images[0]?.url;
 
     return (
-        <div className='bodyHeaderImage' style={{ background: `url(${preview_url})` }}>
-            <div className='bodyHeaderImageGrd'>
-                <div className='bodyHeader'>
-                    <div>
-                        <img src={preview_url || picture} alt='body info picture' />
-                    </div>
-                    <div className='bodyHeader__detail'>
-                        <p>{type || 'Playlists'}</p>
-                        <h2>{name?.substring(0, 20)}</h2>
-                        {description && <p>{description}</p>}
-                        <div className='bodyHeader__other'>
-                            {artists &&
-                                artists?.map((item) => (
-                                    <NavLink
-                                        key={item?.id}
-                                        to={`/artists/${item?.id}`}
-                                        title={item?.type}>
-                                        {item?.name}
-                                    </NavLink>
-                                ))}
-                            {album && (
-                                <NavLink to={`/album/${album?.id}`} title={album?.type}>
-                                    {album?.name}
+        <div style={{ position: 'relative' }}>
+            <div style={{ width: '100%', overflow: 'hidden' }}>
+                <img src={preview_url || picture} alt='bgImage' className='bodyHeaderImage' />
+            </div>
+
+            <div className='bodyHeader'>
+                <div>
+                    <img src={preview_url || picture} alt='body info picture' />
+                </div>
+                <div className='bodyHeader__detail'>
+                    <p>{type || 'Playlists'}</p>
+                    <h2>{name?.substring(0, 20)}</h2>
+                    {description && <p>{description}</p>}
+                    <div className='bodyHeader__other'>
+                        {artists &&
+                            artists?.map((item) => (
+                                <NavLink
+                                    key={item?.id}
+                                    to={`/artists/${item?.id}`}
+                                    title={item?.type}>
+                                    {item?.name}
                                 </NavLink>
-                            )}
-                            {album?.album_release_date && (
-                                <span>
-                                    {convertDate(album?.album_release_date || '2023-02-29')}
-                                </span>
-                            )}
-                            {owner && (
-                                <NavLink to={owner.external_urls.spotify} target='__blank'>
-                                    {owner?.display_name}
-                                </NavLink>
-                            )}
-                            {followers && (
-                                <p>
-                                    {followers?.total?.toLocaleString('en-us', Number) + ' likes'}
-                                </p>
-                            )}
-                            {popularity && <p>{popularity} K</p>}
-                            {duration_ms && <p>{convertTime(duration_ms)} Hours</p>}
-                            {length && <p>{length} Songs</p>}
-                        </div>
+                            ))}
+                        {album && (
+                            <NavLink to={`/album/${album?.id}`} title={album?.type}>
+                                {album?.name}
+                            </NavLink>
+                        )}
+                        {album?.album_release_date && (
+                            <span>{convertDate(album?.album_release_date || '2023-02-29')}</span>
+                        )}
+                        {owner && (
+                            <NavLink to={owner.external_urls.spotify} target='__blank'>
+                                {owner?.display_name}
+                            </NavLink>
+                        )}
+                        {followers && (
+                            <p>{followers?.total?.toLocaleString('en-us', Number) + ' likes'}</p>
+                        )}
+                        {popularity && <p>{popularity} K</p>}
+                        {duration_ms && <p>{convertTime(duration_ms)} Hours</p>}
+                        {length && <p>{length} Songs</p>}
                     </div>
                 </div>
-                {/* Songs Player */}
-                <IFrame/>
             </div>
         </div>
     );
